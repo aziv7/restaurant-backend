81938e481aa3b31fb7c875befaef568d32f162f4
jihen1998 <57370028+jihen1998@users.noreply.github.com>
2021-06-12 20:48:56 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/jihen' into main""
    This reverts commit 810f7d5ea8e190b20f57164f9a4c2ae83c6d1c74.

---
+++ restaurant-backend/app/Http/Controllers/CommandeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Commande;
+use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+
+class CommandeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return  Commande::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'user_id' => 'required',
+            'plat_id' => 'required',
+           'quantite'=> 'required',
+        ]);
+return Commande::create($request->all()) ;   }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+return Commande::find($id) ;   }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $Command= Commande::find($id) ;
+        $Command->update($request->all());
+return $Command;  }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Commande::destroy($id);
+    }
+
+}
--- restaurant-backend/app/Models/CodeReduction.php
+++ restaurant-backend/app/Models/CodeReduction.php
@@ -5,26 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-class Plat extends Model
-{
+
+class CodeReduction extends Model
+{    use SoftDeletes;
     use HasFactory;
-    use SoftDeletes;
     protected $fillable = [
-        'nom',
-        'prix',
-        'description'
+        'code',
+        'taux_reduction',
+        'statut',
     ];
-
-    public function categorie()
-    {
-        return $this->HasOneOrZero (Categorie::class);
-    }
-
-    public function modificateurs()
-    {
-        return $this->belongsToMany(Modificateur::class);
-    }
-
     public function Commandes()
     {
         return $this->hasMany(Commande::class);
