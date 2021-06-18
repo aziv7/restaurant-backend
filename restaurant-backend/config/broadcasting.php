7929521a42c435c8d2e3585517838bb2cb8a87f0
aziv07 <aziv07@gmail.com>
2021-06-18 19:06:40 +0100

    backend config

--- restaurant-backend/config/broadcasting.php
+++ restaurant-backend/config/broadcasting.php
@@ -2,61 +2,16 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Broadcaster
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default broadcaster that will be used by the
-    | framework when an event needs to be broadcast. You may set this to
-    | any of the connections defined in the "connections" array below.
-    |
-    | Supported: "pusher", "ably", "redis", "log", "null"
-    |
-    */
-
-    'default' => env('BROADCAST_DRIVER', 'null'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Broadcast Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the broadcast connections that will be used
-    | to broadcast events to other systems or over websockets. Samples of
-    | each available type of connection are provided inside this array.
-    |
-    */
+    'default' => env('BROADCAST_DRIVER', 'mercure'),
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-            ],
-        ],
+        // ...
 
-        'ably' => [
-            'driver' => 'ably',
-            'key' => env('ABLY_KEY'),
-        ],
-
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
-        ],
-
-        'log' => [
-            'driver' => 'log',
-        ],
-
-        'null' => [
-            'driver' => 'null',
+        'mercure' => [
+            'driver' => 'mercure',
+            'url' => env('MERCURE_URL', 'http://localhost:3000/.well-known/mercure'),
+            'secret' => env('MERCURE_SECRET', 'aVerySecretKey'),
         ],
 
     ],
--- restaurant-backend/routes/channels.php
+++ restaurant-backend/routes/channels.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    //return (int) $user->id === (int) $id;
+    return true;
+});
+
+Broadcast::channel('channel123', function ($user, $id) {
+    //return (int) $user->id === (int) $id;
+    return true;
 });
