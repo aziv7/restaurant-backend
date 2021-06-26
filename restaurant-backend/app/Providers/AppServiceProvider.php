e904b4296b6be1b79f8e842d6b81a6581e44f331
jihen1998 <57370028+jihen1998@users.noreply.github.com>
2021-06-26 23:32:06 +0100

    add role automatically

--- restaurant-backend/app/Providers/AppServiceProvider.php
+++ restaurant-backend/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\Role;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Throwable;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,7 +25,19 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {    $role = new Role();
+    {try {
+        if(!Role::where(['nom_des_roles' =>'costumer'])->first())
+        {$role = new Role();
+            $role->Nom_des_roles='costumer';}
+        if(!Role::where(['nom_des_roles' =>'admin'])->first())
+        {$role_a = new Role();
+            $role_a->Nom_des_roles='admin';
+        }
+        $role->save();$role_a->save();    }
+    catch (Throwable $e) {
+var_dump('c bon');
+    }
+
         Schema::defaultStringLength(191);
     }
 }
