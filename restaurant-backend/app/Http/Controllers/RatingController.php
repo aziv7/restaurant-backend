00995c9eccfc41611c52fe5d488a3aa47126ec3f
jihen1998 <57370028+jihen1998@users.noreply.github.com>
2021-07-22 23:50:57 +0100

    rating affectation

--- restaurant-backend/app/Http/Controllers/RatingController.php
+++ restaurant-backend/app/Http/Controllers/RatingController.php
@@ -13,10 +13,26 @@ class RatingController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
+    { Rating::all();
     }
-
+public function AffectRatingToUser(request $request){
+    $rating=$request->rate;
+    $rating=Rating::where('user_id', 'like', $request->user_id)->where('plat_id', 'like', $request->plat_id)->get()->first();
+    //var_dump($rating);
+    if($rating)
+    {
+        $editdata = array(
+            'note'=>$request->note
+        );
+      return  $rating->update($editdata);
+    }
+    $editdata = array(
+        'note'=>$request->note,
+        'user_id'=>$request->user_id,
+        'plat_id'=>$request->plat_id
+    );
+    return Rating::create($editdata);
+}
     /**
      * Store a newly created resource in storage.
      *
